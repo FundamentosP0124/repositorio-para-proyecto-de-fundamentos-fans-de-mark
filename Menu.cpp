dee52607d22d0cce29d739417ba471ab45c9c6b5
Moises Mencos <00146324@uca.edu.sv>
2024-06-26 09:26:18 -0600

    declaracion de variables

---
+++ Menu.cpp
@@ -0,0 +1,8 @@
+int main() {
+    int opcion;              // declaracion de variables de tipo entero y de tipo string
+    string Usuario_1, Usuario_2;
+    pair<string, string> RESULT;
+    vector<Participant> Participantes;
+    vector<string> JUEGOS;
+
+}

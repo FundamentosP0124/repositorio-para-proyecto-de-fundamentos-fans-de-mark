b053250a38879f11926c89e5ea0b0c33a581791f
Moises Mencos <00146324@uca.edu.sv>
2024-06-26 09:53:50 -0600

    agregando opciones del menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,3 +1,5 @@
+#include <Funciones.hpp>
+
 int main() {
     int opcion;              // declaracion de variables de tipo entero y de tipo string
     string Usuario_1, Usuario_2;
@@ -5,4 +7,21 @@ int main() {
     vector<Participant> Participantes;
     vector<string> JUEGOS;
 
+    do                  // opciones del menu
+    {   
+        cout << "\n                    |||JUEGO X ^ O|||\n";
+        cout << "\n      <=||=||=> OPCIONES DEL JUEGO <=||=||=>\n\n";
+        cout << "1. Inscribir Participante\n";
+        cout << "2. Listas de Puntuaciones de 'jugadores'\n";
+        cout << "3. Jugar\n";
+        cout << "4. Mostrar Ganador o Empate\n";
+        cout << "5. Ayuda\n";
+        cout << "6. Integrantes del Proyecto\n";
+        cout << "7. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+    } while (opcion != 7);
+    
+    return 0;
 }
